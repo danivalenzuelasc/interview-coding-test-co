77db3b26f2e682a780674466c034cc3006e1156c
Daniel Valenzuela <daniel.valenzuela@softcrum.com>
2019-08-16 18:57:41 -0400

    Add model Product

---
+++ src/model/product.js
@@ -0,0 +1,15 @@
+// Declare dependencies
+const uuid = require('uuid/v4');
+
+// Declare model Product
+class Product {
+  constructor(name, sellIn, price) {
+    this.id = uuid();
+    this.name = name;
+    this.price = price;
+    this.sellIn = sellIn;
+  }
+}
+
+// Export Product
+module.exports = Product;
