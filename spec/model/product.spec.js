4db0d351167eca456d51ea406523ea71192e345e
Daniel Valenzuela <daniel.valenzuela@softcrum.com>
2019-08-16 23:59:18 -0400

    Add model CarInsurance

--- spec/model/product.spec.js
+++ spec/model/product.spec.js
@@ -10,7 +10,6 @@ it('Verify if the consumer called the class constructor', () => {
 it('Verify model structure', () => {
   const product = new Product(mocks[0].name, mocks[0].sellIn, mocks[0].price);
   expect(product).toBeDefined();
-  expect(product.id).toBeDefined();
   expect(product.name).toEqual(mocks[0].name);
   expect(product.price).toEqual(mocks[0].price);
   expect(product.sellIn).toEqual(mocks[0].sellIn);
@@ -18,7 +17,6 @@ it('Verify model structure', () => {
 it('Verify class attributes', () => {
   const product = new Product(mocks[1].name, mocks[1].sellIn, mocks[1].price);
   expect(product).toBeDefined();
-  expect(typeof product.id).toEqual('string');
   expect(typeof product.name).toEqual('string');
   expect(typeof product.price).toEqual('number');
   expect(typeof product.sellIn).toEqual('number');
--- src/model/product.js
+++ src/model/product.js
@@ -1,10 +1,6 @@
-// Declare dependencies
-const uuid = require('uuid/v4');
-
 // Declare model Product
 class Product {
   constructor(name, sellIn, price) {
-    this.id = uuid();
     this.name = name;
     this.price = price;
     this.sellIn = sellIn;
